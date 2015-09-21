ea6946995489fac3f2685f4094489823e83565b8
pieszynski <git@pieszynski.com>
2015-09-21 21:40:45 +0200

    Animacje na kliknięcie i dotknięcie

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # KotmanGO
-
-
+Gra logiczna w przeglądarce sylizowanej na aplikację.
 
 #### Zasoby
 [Lista Material icons](https://www.google.com/design/icons/) ([Źródło](https://github.com/google/material-design-icons))
---
+++ www/js/index.js
@@ -0,0 +1,60 @@
+var simpleQuery = (function (document, undefined) {
+    'use strict';
+    function $(selector) {
+        var found = document.querySelector(selector);
+        return found;
+    }
+
+    $.on = function onAttachEvent(obj, eventName, callback) {
+        obj.addEventListener(eventName, callback, false);
+    }
+
+    $.cssAdd = function onCssClassAdd(obj, cssClass) {
+        obj.classList.add(cssClass);
+    }
+
+    $.cssRemove = function onCssClassRemove(obj, cssClass) {
+        obj.classList.remove(cssClass);
+    }
+
+    $.cssToggle = function onCssClassToggle(obj, cssClass) {
+        obj.classList.toggle(cssClass);
+    }
+
+    return $;
+})(document);
+
+(function($, undefined) {
+    "use strict";
+
+    var aSquare = $('.a'),
+        bCircle = $('.b');
+
+    //$.cssAdd(aSquare, 'platform-pops');
+    //$.cssAdd(bCircle, 'platform-pops');
+
+    $.on(aSquare, 'animationstart', onAnimationStart);
+    $.on(aSquare, 'animationend', onAnimationTouchEnd);
+    $.on(aSquare, 'click', onElementTouch);
+    $.on(aSquare, 'touchend', onElementTouch);
+
+    $.on(bCircle, 'animationstart', onAnimationStart);
+    $.on(bCircle, 'animationend', onAnimationTouchEnd);
+    $.on(bCircle, 'click', onElementTouch);
+    $.on(bCircle, 'touchend', onElementTouch);
+
+    function onAnimationStart(evt) {
+        $.cssAdd(this, 'platform-final');
+    }
+
+    function onAnimationTouchEnd(evt) {
+        $.cssRemove(this, 'touchme');
+        $.cssRemove(this, 'platform-pops');
+        //console.log('anim end', evt.animationName, evt, this);
+    }
+
+    function onElementTouch(evt) {
+        $.cssAdd(this, 'touchme');
+    }
+
+})(simpleQuery);
